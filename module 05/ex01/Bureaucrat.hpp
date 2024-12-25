f60b43f4514e1f740baff4abda1630a2dc021f2b
zelhajou <zakaria.elhajoui1@gmail.com>
2024-12-25 17:50:10 +0100

    ex01

--- module 05/ex01/Bureaucrat.hpp
+++ module 05/ex01/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 16:26:56 by zelhajou          #+#    #+#             */
-/*   Updated: 2024/12/22 16:40:34 by zelhajou         ###   ########.fr       */
+/*   Updated: 2024/12/25 15:25:16 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,39 @@
 
 class Form;
 
-class Bureaucrat {
+class Bureaucrat
+{
 private:
-    const std::string   _name;
-    int                 _grade;
+    const std::string _name;
+    int _grade;
 
 public:
     Bureaucrat();
     Bureaucrat(const std::string &name, int grade);
-    Bureaucrat(const Bureaucrat& src);
+    Bureaucrat(const Bureaucrat &src);
     ~Bureaucrat();
-    Bureaucrat& operator=(const Bureaucrat& src);
+    Bureaucrat &operator=(const Bureaucrat &src);
 
-    const std::string&  getName() const;
-    int                 getGrade() const;
+    const std::string &getName() const;
+    int getGrade() const;
 
     void incrementGrade();
     void decrementGrade();
-    void signForm(Form& form);
+    void signForm(Form &form);
 
-    class GradeTooHighException : public std::exception {
-        public:
-            const char* what() const throw();
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        const char *what() const throw();
     };
-    
-    class GradeTooLowException : public std::exception {
-        public:
-            const char* what() const throw();
+
+    class GradeTooLowException : public std::exception
+    {
+    public:
+        const char *what() const throw();
     };
 };
 
-std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
 
 #endif
--- module 05/ex01/main.cpp
+++ module 05/ex01/main.cpp
@@ -6,61 +6,62 @@
 /*   By: zelhajou <zelhajou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 16:31:49 by zelhajou          #+#    #+#             */
-/*   Updated: 2024/12/22 16:34:04 by zelhajou         ###   ########.fr       */
+/*   Updated: 2024/12/25 17:48:59 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
+#include <vector>
 
 int main()
 {
-    try {
+    try
+    {
         std::cout << "\n-------- Test 1: Valid Form Creation --------\n";
         Form form1("Tax Form", 50, 30);
         std::cout << form1 << std::endl;
 
         std::cout << "\n-------- Test 2: Invalid Grade (Too High) --------\n";
-        try {
+        try
+        {
             Form form2("Invalid Form", 0, 30);
         }
-        catch(const std::exception& e) {
+        catch (const std::exception &e)
+        {
             std::cout << "Exception: " << e.what() << std::endl;
         }
 
         std::cout << "\n-------- Test 3: Invalid Grade (Too Low) --------\n";
-        try {
+        try
+        {
             Form form3("Invalid Form", 50, 151);
         }
-        catch(const std::exception& e) {
+        catch (const std::exception &e)
+        {
             std::cout << "Exception: " << e.what() << std::endl;
         }
 
         std::cout << "\n-------- Test 4: Form Signing (Success) --------\n";
-        Bureaucrat bob("Bob", 40);
+        Bureaucrat sherif("Sherif", 40);
         Form form4("Contract", 50, 30);
-        std::cout << "Before signing:\n" << form4 << std::endl;
-        bob.signForm(form4);
-        std::cout << "After signing:\n" << form4 << std::endl;
+        std::cout << "Before signing:\n"
+                  << form4 << std::endl;
+        sherif.signForm(form4);
+        std::cout << "After signing:\n"
+                  << form4 << std::endl;
 
         std::cout << "\n-------- Test 5: Form Signing (Failure) --------\n";
-        Bureaucrat jim("Jim", 100);
+        Bureaucrat sherifa("Sherifa", 100);
         Form form5("Classified Document", 20, 10);
-        std::cout << "Before signing:\n" << form5 << std::endl;
-        jim.signForm(form5);
-        std::cout << "After signing attempt:\n" << form5 << std::endl;
-
-        std::cout << "\n-------- Test 6: Form Copy --------\n";
-        Form form6(form4);  // Copy constructor
-        std::cout << "Copied form:\n" << form6 << std::endl;
-
-        std::cout << "\n-------- Test 7: Form Assignment --------\n";
-        Form form7("Empty Form", 100, 100);
-        form7 = form4;  // Assignment operator
-        std::cout << "Assigned form:\n" << form7 << std::endl;
-
+        std::cout << "Before signing:\n"
+                  << form5 << std::endl;
+        sherifa.signForm(form5);
+        std::cout << "After signing attempt:\n"
+                  << form5 << std::endl;
     }
-    catch (const std::exception& e) {
+    catch (const std::exception &e)
+    {
         std::cout << "Unexpected exception: " << e.what() << std::endl;
     }
 
