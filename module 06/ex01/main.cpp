c463d44dbb13555ef2b1223d749c63a3214d93ba
zelhajou <zakaria.elhajoui1@gmail.com>
2025-01-01 22:16:24 +0100

    Update main.cpp with improved formatting and comments

--- module 06/ex01/main.cpp
+++ module 06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 18:20:49 by zelhajou          #+#    #+#             */
-/*   Updated: 2025/01/01 18:20:51 by zelhajou         ###   ########.fr       */
+/*   Updated: 2025/01/01 21:24:51 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 
 int main()
 {
-    Data* data = new Data;
+    Data *data = new Data;
     data->name = "test";
     data->value = 42;
 
-    std::cout << "Original data - Address: " << data 
-              << ", Name: " << data->name 
+    std::cout << "Original data - Address: " << data
+              << ", Name: " << data->name
               << ", Value: " << data->value << std::endl;
 
     uintptr_t serialized = Serializer::serialize(data);
     std::cout << "Serialized: " << serialized << std::endl;
 
-    Data* deserialized = Serializer::deserialize(serialized);
-    std::cout << "Deserialized data - Address: " << deserialized 
-              << ", Name: " << deserialized->name 
+    Data *deserialized = Serializer::deserialize(serialized);
+    std::cout << "Deserialized data - Address: " << deserialized
+              << ", Name: " << deserialized->name
               << ", Value: " << deserialized->value << std::endl;
 
     std::cout << "Pointers are equal: " << (data == deserialized ? "true" : "false") << std::endl;
