f7ea22fb12238080f7947fdc33ab67b6f271b7e5
zelhajou <zakaria.elhajoui1@gmail.com>
2025-01-05 22:48:45 +0100

    Update Span class implementation with copy constructor and assignment operator

--- module 08/ex01/Span.cpp
+++ module 08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: zelhajou <zelhajou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 21:02:48 by zelhajou          #+#    #+#             */
-/*   Updated: 2025/01/05 21:11:06 by zelhajou         ###   ########.fr       */
+/*   Updated: 2025/01/05 22:41:14 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,20 @@
 #include <climits>
 
 Span::Span() : _N(0) {}
+
+Span::Span(unsigned int N) : _N(N) {}
+
+Span::Span(const Span &other) : _N(other._N), _numbers(other._numbers) {}
+
+Span::~Span() {}
+
+Span &Span::operator=(const Span &other)
+{
+    if (this != &other)
+    {
+        _N = other._N;
+        _numbers = other._numbers;
+    }
+    return *this;
+}
 
