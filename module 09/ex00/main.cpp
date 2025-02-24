30629fd4451ff40a9e8b6e81f3874f31b4a6bdc2
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2025-02-24 20:06:55 +0000

    chore: Update main.cpp to handle command line arguments for input file

--- module 09/ex00/main.cpp
+++ module 09/ex00/main.cpp
@@ -1,19 +1,22 @@
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./btc [input_file]" << std::endl;
+        return 1;
+    }
     BitcoinExchange exchange;
-
     try
     {
         exchange.loadDataBase("data.csv");
         // exchange.displayDataBase();
-        exchange.processInputFile("input.txt");
+        exchange.processInputFile(argv[1]);
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
     }
-
     return 0;
 }
